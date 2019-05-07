86b29615d1f1bb487c635cdecf5d25ab65ebe852
Ayaka526 <tatw.m26@gmail.com>
2019-05-07 04:28:58 +0000

    [EDIT]edit/js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,30 +42,6 @@ $(document).ready(function() {
 
 });
 
-$(document).ready(function() {
-  $(function(){
-    $('#search').on('click', function() {
-      $.ajax({
-      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
-      dataType : 'jsonp',
-      }).done(function (data){
-        //success
-        $('#place').text(data.name);
-        $('#temp_max').text(data.main.temp_max);
-        $('#temp_min').text(data.main.temp_min);
-        $('#humidity').text(data.main.humidity);
-        $('#weather').text(data.weather[0].main);
-        $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-        $('img').attr("alt",data.weather[0].main);
-      }).fail(function (data) {
-         //failed
-         alert('通信に失敗しました。');
-      });
-    });
-  });
- });
-
-
 
 
 $(document).ready(function() {
